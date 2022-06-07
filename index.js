fca14334895001c141c2b460ceaa2643e30fd4c6
Elijah <bundielly@gmail.com>
2022-06-07 11:20:13 +0300

    Functions inside a function

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function(doStuff = 'go to the office') {
+    return `This Monday, I will ${doStuff}.`
+}
+
+const wrapAdjective = function(string= '*') {
+    return function(wrap= 'special') {
+        return `You are ${string}${wrap}${string}!`
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
